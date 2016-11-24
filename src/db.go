af6955a92f05428505b1342312451dcb65735228
Lukas Breuer <lukas.breuer@outlook.com>
2016-11-24 01:45:46 +0100

    Add database helpers

--- src/db.go
+++ src/db.go
@@ -1,1 +1,25 @@
 package main
+
+import "gopkg.in/mgo.v2"
+
+var (
+    dbName string
+    dbSession *mgo.Session
+)
+
+func ConnectDB(url string, db string) {
+    dbName = db
+    session, err := mgo.Dial(url)
+
+    if err != nil {
+        panic(err)
+    }
+
+    dbSession = session
+
+    defer session.Close()
+}
+
+func GetDB() *mgo.Database {
+    return dbSession.DB(dbName)
+}
