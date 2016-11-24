94ea6d979c120d8f250056f2acdf62610d8679eb
Lukas Breuer <lukas.breuer@outlook.com>
2016-11-24 01:42:24 +0100

    Add models for Mongodb

---
+++ src/models/config.go
@@ -0,0 +1,13 @@
+package models
+
+import "github.com/bwmarrin/discordgo"
+
+type GuildSettings struct {
+    Guild *discordgo.Guild
+    Settings []GuildSetting
+}
+
+type GuildSetting struct {
+    Key string
+    Value string
+}
