3f2fe6939cdc20061caa7794a819b941299fd20a
Lukas Breuer <lukas.breuer@outlook.com>
2016-12-18 14:40:46 +0100

    Add helper that transforms numbers to words

---
+++ helpers/numbers.go
@@ -0,0 +1,38 @@
+package helpers
+
+func HumanizeNumber(num int) string {
+    switch num {
+    case 1:
+        return "one"
+
+    case 2:
+        return "two"
+
+    case 3:
+        return "three"
+
+    case 4:
+        return "four"
+
+    case 5:
+        return "five"
+
+    case 6:
+        return "six"
+
+    case 7:
+        return "seven"
+
+    case 8:
+        return "eight"
+
+    case 9:
+        return "nine"
+
+    case 0:
+        return "zero"
+
+    default:
+        return ""
+    }
+}
