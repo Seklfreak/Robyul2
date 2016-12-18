cbc68ca28421704d868dbc32a4b7600b26a9767e
Lukas Breuer <lukas.breuer@outlook.com>
2016-12-18 14:41:58 +0100

    Activate reminder migration

--- migrations/migrator.go
+++ migrations/migrator.go
@@ -1,19 +1,30 @@
 package migrations
 
-import "fmt"
+import (
+    "fmt"
+    "runtime"
+    "reflect"
+)
 
 type Migration func()
 
 var Migrations = []Migration{
     M0_create_db,
     M1_create_table_guild_config,
+    M2_create_table_reminders,
 }
 
 // Runs all registered migrations
 func Run() {
     fmt.Println("[DB] Running migrations...")
     for _, migration := range Migrations {
+        migrationName := runtime.FuncForPC(
+            reflect.ValueOf(migration).Pointer(),
+        ).Name()
+
+        fmt.Printf("[DB] Running %s\n", migrationName)
         migration()
     }
+
     fmt.Println("[DB] Migrations finished!")
 }
