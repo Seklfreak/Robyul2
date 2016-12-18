e91a8a2f2422110d817f474de4c4daad3926d4b7
Lukas Breuer <lukas.breuer@outlook.com>
2016-12-18 14:40:18 +0100

    Extend Relax helpers

--- helpers/except.go
+++ helpers/except.go
@@ -2,10 +2,26 @@
 
 package helpers
 
+import "reflect"
+
 // Helper to reduce if-checks if panicking is allowed
 // If $err is nil this is a no-op. Panics otherwise.
 func Relax(err error) {
     if err != nil {
         panic(err)
+    }
+}
+
+// if a != b throw err
+func RelaxAssertEqual(a interface{}, b interface{}, err error) {
+    if !reflect.DeepEqual(a, b) {
+        Relax(err)
+    }
+}
+
+// if a == b throw err
+func RelaxAssertUnequal(a interface{}, b interface{}, err error) {
+    if reflect.DeepEqual(a, b) {
+        Relax(err)
     }
 }
