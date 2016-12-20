e8b886a244d59f88efc2d45faab3d163bf905c34
Lukas Breuer <lukas.breuer@outlook.com>
2016-12-20 21:57:21 +0100

    Add collection helpers

--- helpers/collections.go
+++ helpers/collections.go
@@ -1,14 +1,34 @@
 package helpers
 
-import "reflect"
+import (
+    "reflect"
+    "errors"
+)
 
-// Checks if the slice $s contains $e
-func SliceContains(s []interface{}, e interface{}) bool {
-    for _, a := range s {
-        if reflect.DeepEqual(a, e) {
-            return true
-        }
+func interfaceToSlice(input interface{}) []interface{} {
+    r := reflect.ValueOf(input)
+
+    if r.Kind() != reflect.Slice {
+        panic(errors.New("Passed arg is not a slice"))
     }
 
-    return false
+    c := r.Len()
+    s := make([]interface{}, c)
+
+    for i := 0; i < c; i++ {
+        s[i] = r.Index(i).Interface()
+    }
+
+    return s
+}
+
+func SliceRemoveOrderedElement(input interface{}, idx int) []interface{} {
+    array := interfaceToSlice(input)
+    return append(array[:idx], array[idx + 1:]...)
+}
+
+func SliceRemoveElement(input interface{}, idx int) []interface{} {
+    array := interfaceToSlice(input)
+    array[len(array) - 1], array[idx] = array[idx], array[len(array) - 1]
+    return array[:len(array) - 1]
 }
