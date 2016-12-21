a2fe5f0c7972d7e7eb0a987c74e22073ce1681ea
Lukas Breuer <lukas.breuer@outlook.com>
2016-12-21 17:32:10 +0100

    Remove website

--- website/application/config/migration.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['migration_enabled'] = false;
-$config['migration_type'] = 'timestamp';
-$config['migration_table'] = 'migrations';
-$config['migration_auto_latest'] = false;
-$config['migration_version'] = 0;
-$config['migration_path'] = APPPATH . 'migrations/';
