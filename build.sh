8114ba417f54c34822d60af6793aa6b1f3544b12
Lukas Breuer <lukas.breuer@outlook.com>
2017-01-06 01:13:52 +0100

    Make the build more verbose

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ BUILD_TIME=$(date +%T-%D)
 BUILD_USER="$USER"
 BUILD_HOST=$(hostname)
 
-go build --ldflags="
+go build -v -x --ldflags="
 -X github.com/sn0w/Karen/version.BOT_VERSION=${BOT_VERSION}
 -X github.com/sn0w/Karen/version.BUILD_TIME=${BUILD_TIME}
 -X github.com/sn0w/Karen/version.BUILD_USER=${BUILD_USER}
