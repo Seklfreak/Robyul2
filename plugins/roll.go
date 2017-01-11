9d17d9175194993a57c6903693e88cb0fbe0193d
Lukas Breuer <lukas.breuer@outlook.com>
2017-01-11 19:08:22 +0100

    Catch more errors in roll.go

--- plugins/roll.go
+++ plugins/roll.go
@@ -21,7 +21,7 @@ func (r Roll) Init(session *discordgo.Session) {
 }
 
 func (r Roll) Action(command string, content string, msg *discordgo.Message, session *discordgo.Session) {
-    regex := regexp.MustCompile(`\D`)
+    regex := regexp.MustCompile(`[^\d-]`)
     args := strings.Split(content, " ")
 
     if len(args) == 2 {
@@ -37,11 +37,21 @@ func (r Roll) Action(command string, content string, msg *discordgo.Message, ses
             return
         }
 
+        if min <= 0 || max <= 0 {
+            session.ChannelMessageSend(msg.ChannelID, ":x: Only positive numbers are allowed")
+            return
+        }
+
+        if min >= max {
+            session.ChannelMessageSend(msg.ChannelID, ":x: Number ranges don't work like that. (`min >= max`)")
+            return
+        }
+
         session.ChannelMessageSend(
             msg.ChannelID,
             ":crystal_ball: " + strconv.Itoa(rand.Intn(int(max - min)) + int(min)),
         )
     } else {
-        session.ChannelMessageSend(msg.ChannelID, "Please check if your call was correct :frowning:")
+        session.ChannelMessageSend(msg.ChannelID, ":x: You need to pass two numbers")
     }
 }
