649ec6a21d11463f43ea8fcc35d52e2cf9c8a782
Lukas Breuer <lukas.breuer@outlook.com>
2017-01-11 18:13:23 +0100

    Add support command

---
+++ plugins/support.go
@@ -0,0 +1,19 @@
+package plugins
+
+import "github.com/bwmarrin/discordgo"
+
+type Support struct{}
+
+func (s Support) Commands() []string {
+    return []string{
+        "support",
+    }
+}
+
+func (s Support) Init(session *discordgo.Session) {
+
+}
+
+func (s Support) Action(command string, content string, msg *discordgo.Message, session *discordgo.Session) {
+    session.ChannelMessageSend(msg.ChannelID, "Here you go :smiley: \n https://discord.gg/wNPejct")
+}
