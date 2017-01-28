6ec9f17bffb5f43b244455f167862c20faa1a3b8
Lukas Breuer <lukas.breuer@outlook.com>
2017-01-28 22:53:00 +0100

    Remove unused comment

--- plugins/triggers/iptables.go
+++ plugins/triggers/iptables.go
@@ -2,10 +2,6 @@ package triggers
 
 import "git.lukas.moe/sn0w/Karen/helpers"
 
-/**
- * Full credit to Der-Eddy and his original python implementation for Shinobu-Chan.
- * @link https://github.com/Der-Eddy/discord_bot
- */
 type IPTables struct {}
 
 func (i IPTables) Triggers() []string {
