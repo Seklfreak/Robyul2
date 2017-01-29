3f6ae290462c4a4ebe7dd70613c6e42db0671136
Lukas Breuer <lukas.breuer@outlook.com>
2017-01-29 18:31:22 +0100

    Add message templates to more plugins

--- plugins/avatar.go
+++ plugins/avatar.go
@@ -3,6 +3,7 @@ package plugins
 import (
     "fmt"
     "github.com/sn0w/discordgo"
+    "git.lukas.moe/sn0w/Karen/helpers"
 )
 
 type Avatar struct{}
@@ -18,13 +19,21 @@ func (a Avatar) Init(session *discordgo.Session) {
 }
 
 func (a Avatar) Action(command string, content string, msg *discordgo.Message, session *discordgo.Session) {
-    if len(msg.Mentions) > 0 {
-        session.ChannelMessageSend(msg.ChannelID, "Here you go :smiley: \n " + fmt.Sprintf(
-            "https://cdn.discordapp.com/avatars/%s/%s.jpg",
-            msg.Mentions[0].ID,
-            msg.Mentions[0].Avatar,
-        ))
-    } else {
-        session.ChannelMessageSend(msg.ChannelID, "You should mention a user :thinking:")
+    mentionCount := len(msg.Mentions)
+
+    if mentionCount == 0 {
+        session.ChannelMessageSend(msg.ChannelID, helpers.GetText("bot.mentions.too-few"))
+        return
     }
+
+    if mentionCount > 1 {
+        session.ChannelMessageSend(msg.ChannelID, helpers.GetText("bot.mentions.too-many"))
+        return
+    }
+
+    session.ChannelMessageSend(msg.ChannelID, "Here you go :smiley: \n " + fmt.Sprintf(
+        "https://cdn.discordapp.com/avatars/%s/%s.jpg",
+        msg.Mentions[0].ID,
+        msg.Mentions[0].Avatar,
+    ))
 }
--- plugins/stone.go
+++ plugins/stone.go
@@ -3,6 +3,7 @@ package plugins
 import (
     "fmt"
     "github.com/sn0w/discordgo"
+    "git.lukas.moe/sn0w/Karen/helpers"
 )
 
 type Stone struct{}
@@ -18,6 +19,18 @@ func (s Stone) Init(session *discordgo.Session) {
 }
 
 func (s Stone) Action(command string, content string, msg *discordgo.Message, session *discordgo.Session) {
+    mentionCount := len(msg.Mentions)
+
+    if mentionCount == 0 {
+        session.ChannelMessageSend(msg.ChannelID, helpers.GetText("bot.mentions.too-few"))
+        return
+    }
+
+    if mentionCount > 1 {
+        session.ChannelMessageSend(msg.ChannelID, helpers.GetText("bot.mentions.too-many"))
+        return
+    }
+
     session.ChannelMessageSend(msg.ChannelID, fmt.Sprintf(
         "<@%s> IS GOING TO DIE!!!\n" +
             "COME ON GUYS! THROW SOME STONES WITH MEE!!!\n" +
