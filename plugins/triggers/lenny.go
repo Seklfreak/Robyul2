83f1fdc8f1e17e8a671eba22fd3a7551a42f9a26
Lukas Breuer <lukas.breuer@outlook.com>
2017-02-04 20:23:02 +0100

    Turn lenny into a trigger

---
+++ plugins/triggers/lenny.go
@@ -0,0 +1,13 @@
+package triggers
+
+type Lenny struct{}
+
+func (l Lenny) Triggers() []string {
+    return []string{
+        "lenny",
+    }
+}
+
+func (l Lenny) Response(trigger string, content string) string {
+    return "( ͡° ͜ʖ ͡°)"
+}
