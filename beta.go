faad78414b40743a1973ee6c77e5e4b91dd2673a
Lukas Breuer <lukas.breuer@outlook.com>
2017-02-16 19:02:37 +0100

    Whitelist omurice

--- beta.go
+++ beta.go
@@ -21,6 +21,7 @@ var BETA_GUILDS = [...]string{
     "268143270520029187", // Joel's Beasts          (Joel)
     "271346578189582339", // Universe Internet Ltd. (Inside24)
     "270353850085408780", // Turdy Republic         (Moopdedoop)
+    "275720670045011968", // Omurice                (Katsurice)
 }
 
 // Automatically leaves guilds that are not registered beta testers
