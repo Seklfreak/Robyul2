69771099ac52c3c137769ccd6adbe4a20e6bf656
Lukas Breuer <lukas.breuer@outlook.com>
2017-02-25 17:59:30 +0100

    Add API error message

--- helpers/cleverbot.go
+++ helpers/cleverbot.go
@@ -22,7 +22,14 @@ func CleverbotSend(session *discordgo.Session, channel string, message string) {
 
     response, err := cleverbotSessions[channel].Ask(message)
     if err != nil {
-        msg = "Error :frowning:\n```\n" + err.Error() + "\n```"
+        if err == cleverbot.ErrTooManyRequests {
+            msg = "I cannot talk to you right now. :speak_no_evil:\n" +
+                "CleverBot costs money, and the plan I'm currently on has no requests left.\n" +
+                "If you want to help 0xFADED buying larger plans, cosider making a donation on his patreon. :innocent:\n" +
+                "Link: <https://www.patreon.com/sn0w>"
+        } else {
+            msg = "Error :frowning:\n```\n" + err.Error() + "\n```"
+        }
     } else {
         msg = response
     }
