40056bfad805fe9d2f34363a32cd78a1f49ccc38
Sebastian Winkler <sekl@slmn.de>
2017-02-27 14:35:28 +0100

    [mod] adds user id support for [p]mute and [p]unmute

--- plugins/mod.go
+++ plugins/mod.go
@@ -133,48 +133,48 @@ func (m *Mod) Action(command string, content string, msg *discordgo.Message, ses
 				}
 			}
 		})
-	case "mute": // [p]mute server <@User>
+	case "mute": // [p]mute server <User>
 		helpers.RequireAdmin(msg, func() {
 			args := strings.Split(content, " ")
 			if len(args) >= 2 {
-				if len(msg.Mentions) >= 1 {
-					switch args[0] {
-					case "server":
-						channel, err := session.Channel(msg.ChannelID)
-						helpers.Relax(err)
-						muteRole, err := helpers.GetMuteRole(channel.GuildID)
-						helpers.Relax(err)
-						err = session.GuildMemberRoleAdd(channel.GuildID, msg.Mentions[0].ID, muteRole.ID)
-						helpers.Relax(err)
-						session.ChannelMessageSend(msg.ChannelID, helpers.GetTextF("plugins.mod.user-muted-success", msg.Author.Username))
-					}
-				} else {
+				targetUser, err := helpers.GetUserFromMention(args[1])
+				if err != nil {
 					session.ChannelMessageSend(msg.ChannelID, helpers.GetTextF("bot.arguments.invalid"))
 					return
+				}
+				switch args[0] {
+				case "server":
+					channel, err := session.Channel(msg.ChannelID)
+					helpers.Relax(err)
+					muteRole, err := helpers.GetMuteRole(channel.GuildID)
+					helpers.Relax(err)
+					err = session.GuildMemberRoleAdd(channel.GuildID, targetUser.ID, muteRole.ID)
+					helpers.Relax(err)
+					session.ChannelMessageSend(msg.ChannelID, helpers.GetTextF("plugins.mod.user-muted-success", targetUser.Username, targetUser.ID))
 				}
 			} else {
 				session.ChannelMessageSend(msg.ChannelID, helpers.GetTextF("bot.arguments.too-few"))
 				return
 			}
 		})
-	case "unmute": // [p]unmute server <@User>
+	case "unmute": // [p]unmute server <User>
 		helpers.RequireAdmin(msg, func() {
 			args := strings.Split(content, " ")
 			if len(args) >= 2 {
-				if len(msg.Mentions) >= 1 {
-					switch args[0] {
-					case "server":
-						channel, err := session.Channel(msg.ChannelID)
-						helpers.Relax(err)
-						muteRole, err := helpers.GetMuteRole(channel.GuildID)
-						helpers.Relax(err)
-						err = session.GuildMemberRoleRemove(channel.GuildID, msg.Mentions[0].ID, muteRole.ID)
-						helpers.Relax(err)
-						session.ChannelMessageSend(msg.ChannelID, helpers.GetTextF("plugins.mod.user-unmuted-success", msg.Author.Username))
-					}
-				} else {
+				targetUser, err := helpers.GetUserFromMention(args[1])
+				if err != nil {
 					session.ChannelMessageSend(msg.ChannelID, helpers.GetTextF("bot.arguments.invalid"))
 					return
+				}
+				switch args[0] {
+				case "server":
+					channel, err := session.Channel(msg.ChannelID)
+					helpers.Relax(err)
+					muteRole, err := helpers.GetMuteRole(channel.GuildID)
+					helpers.Relax(err)
+					err = session.GuildMemberRoleRemove(channel.GuildID, targetUser.ID, muteRole.ID)
+					helpers.Relax(err)
+					session.ChannelMessageSend(msg.ChannelID, helpers.GetTextF("plugins.mod.user-unmuted-success", targetUser.Username, targetUser.ID))
 				}
 			} else {
 				session.ChannelMessageSend(msg.ChannelID, helpers.GetTextF("bot.arguments.too-few"))
