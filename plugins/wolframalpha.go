2208d767bb252aae248f9ce1d936f9cde02b8930
Sebastian Winkler <sekl@slmn.de>
2017-02-28 16:35:31 +0100

    [wolframalpha] better output

--- plugins/wolframalpha.go
+++ plugins/wolframalpha.go
@@ -6,12 +6,15 @@ import (
 	"github.com/Seklfreak/Robyul2/metrics"
 	"github.com/bwmarrin/discordgo"
 	"net/url"
+	"strings"
 )
 
 type WolframAlpha struct{}
 
 const (
-	wolframBaseUrl string = "http://api.wolframalpha.com/v1/result?units=metric&appid=%s&i=%s"
+	wolframBaseUrl       string = "http://api.wolframalpha.com/v2/query?units=metric&output=json&appid=%s&input=%s"
+	wolframFriendlyUrl   string = "http://www.wolframalpha.com/input/?i=%s"
+	wolframalphaHexColor string = "#ff8737"
 )
 
 func (m *WolframAlpha) Commands() []string {
@@ -36,10 +39,48 @@ func (m *WolframAlpha) Action(command string, content string, msg *discordgo.Mes
 	}()
 	session.ChannelTyping(msg.ChannelID)
 
-	queryUrl := fmt.Sprintf(wolframBaseUrl, helpers.GetConfig().Path("wolframalpha.appid").Data().(string), url.QueryEscape(content))
+	encodedQuery := url.QueryEscape(content)
+	queryUrl := fmt.Sprintf(wolframBaseUrl, helpers.GetConfig().Path("wolframalpha.appid").Data().(string), encodedQuery)
 
-	result := helpers.NetGet(queryUrl)
+	result := helpers.GetJSON(queryUrl)
 
-	session.ChannelMessageSend(msg.ChannelID, string(result))
+	podResultItems, err := result.Path("queryresult.pods").Children()
+	helpers.Relax(err)
+
+	resultEmbed := &discordgo.MessageEmbed{
+		Title:  helpers.GetTextF("plugins.wolframalpha.result-embed-title", content),
+		URL:    fmt.Sprintf(wolframFriendlyUrl, encodedQuery),
+		Footer: &discordgo.MessageEmbedFooter{Text: helpers.GetText("plugins.wolframalpha.embed-footer")},
+		Fields: []*discordgo.MessageEmbedField{},
+		Color:  helpers.GetDiscordColorFromHex(wolframalphaHexColor),
+	}
+
+	for _, podResult := range podResultItems {
+		isPrimary, ok := podResult.Path("primary").Data().(bool)
+		if ok == false || isPrimary == false {
+			continue
+		}
+
+		titleText := podResult.Path("title").Data().(string)
+		valueText := ""
+		subPodResultItems, err := podResult.Path("subpods").Children()
+		helpers.Relax(err)
+		for _, subPodResult := range subPodResultItems {
+			for _, line := range strings.Split(subPodResult.Path("plaintext").Data().(string), "|") {
+				if line != "" {
+					valueText += strings.Trim(line, " ") + "; "
+				}
+			}
+		}
+		if valueText != "" {
+			resultEmbed.Fields = append(resultEmbed.Fields, &discordgo.MessageEmbedField{
+				Name:   titleText,
+				Value:  valueText,
+				Inline: false,
+			})
+		}
+	}
+
+	session.ChannelMessageSendEmbed(msg.ChannelID, resultEmbed)
 	metrics.WolframAlphaRequests.Add(1)
 }
