d10661197a49cbcb9621402cf29decfd8f03afb4
Juan Álvarez <shixzie@gmail.com>
2017-03-04 08:21:01 -0400

    using spaces

--- modules/triggers/kawaii.go
+++ modules/triggers/kawaii.go
@@ -7,12 +7,12 @@ type Kawaii struct{}
 
 // Triggers cmds
 func (k Kawaii) Triggers() []string {
-	return []string{
-		"kawaii",
-	}
+    return []string{
+        "kawaii",
+    }
 }
 
 // Response with a kawaii pic link
 func (k Kawaii) Response(trigger string, content string) string {
-	return helpers.GetText("triggers.kawaii.link")
+    return helpers.GetText("triggers.kawaii.link")
 }
