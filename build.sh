4f33a65c114ed1282027e22e3b885962e0799c23
Lukas Breuer <lukas.breuer@outlook.com>
2017-03-04 22:20:03 +0100

    Detect race conditions in debug builds

--- build.sh
+++ build.sh
@@ -12,13 +12,16 @@ if [[ "$CI" == "true" ]]; then
     GOTARGET="${GOTARGET?:'A target is mandatory'}"
 else
     GOTARGET="Karen"
+    XFLAGS="${XFLAGS} -race"
 fi
 
 echo $GOTARGET
 
 go-bindata -nomemcopy -nocompress -pkg helpers -o helpers/assets.go _assets/
-go build ${XFLAGS} --ldflags="
+go build ${XFLAGS} \
+    -o "${GOTARGET}" \
+    --ldflags="
 -X git.lukas.moe/sn0w/Karen/version.BOT_VERSION=${BOT_VERSION}
 -X git.lukas.moe/sn0w/Karen/version.BUILD_TIME=${BUILD_TIME}
 -X git.lukas.moe/sn0w/Karen/version.BUILD_USER=${BUILD_USER}
--X git.lukas.moe/sn0w/Karen/version.BUILD_HOST=${BUILD_HOST}" -o ${GOTARGET} .
+-X git.lukas.moe/sn0w/Karen/version.BUILD_HOST=${BUILD_HOST}" .
