611842fbe9e233b16235ec80914fa54fbc055d5e
Lukas Breuer <lukas.breuer@outlook.com>
2017-03-07 00:00:06 +0100

    [build] Shorten script

--- build.sh
+++ build.sh
@@ -6,20 +6,18 @@ BOT_VERSION=$(git describe --tags)
 BUILD_TIME=$(date +%T-%D)
 BUILD_USER="$USER"
 BUILD_HOST=$(hostname)
-CFLAGS=""
 
 if [[ "$CI" == "true" ]]; then
     GOTARGET="${GOTARGET?:'A target is mandatory'}"
 else
     GOTARGET="Karen"
-    CFLAGS="${CFLAGS} ${@}"
 fi
 
 set -x
 
 go-bindata -nomemcopy -nocompress -pkg helpers -o helpers/assets.go _assets/
 
-go build ${CFLAGS} \
+go build ${@} \
     -o "${GOTARGET}" \
     --ldflags="
 -X git.lukas.moe/sn0w/Karen/version.BOT_VERSION=${BOT_VERSION}
