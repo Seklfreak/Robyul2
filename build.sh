fd44c833d18eaa5626605fb7f08200d04d83eb52
Lukas Breuer <lukas.breuer@outlook.com>
2017-03-06 23:53:50 +0100

    [build] Pass script arguments to "go build"

--- build.sh
+++ build.sh
@@ -6,22 +6,24 @@ BOT_VERSION=$(git describe --tags)
 BUILD_TIME=$(date +%T-%D)
 BUILD_USER="$USER"
 BUILD_HOST=$(hostname)
-XFLAGS="-v"
+CFLAGS=""
 
 if [[ "$CI" == "true" ]]; then
     GOTARGET="${GOTARGET?:'A target is mandatory'}"
 else
     GOTARGET="Karen"
-    XFLAGS="${XFLAGS} -race"
+    CFLAGS="${CFLAGS} ${@}"
 fi
 
-echo $GOTARGET
+set -x
 
 go-bindata -nomemcopy -nocompress -pkg helpers -o helpers/assets.go _assets/
-go build ${XFLAGS} \
+
+go build ${CFLAGS} \
     -o "${GOTARGET}" \
     --ldflags="
 -X git.lukas.moe/sn0w/Karen/version.BOT_VERSION=${BOT_VERSION}
 -X git.lukas.moe/sn0w/Karen/version.BUILD_TIME=${BUILD_TIME}
 -X git.lukas.moe/sn0w/Karen/version.BUILD_USER=${BUILD_USER}
 -X git.lukas.moe/sn0w/Karen/version.BUILD_HOST=${BUILD_HOST}" .
+
