3439705ee6e28828403a6b6fa906061b166245ee
Lomand <sergeyzhukaev@yandex.ru>
2017-03-12 02:48:23 +0300

    #36 Headpat images

--- modules/modules.go
+++ modules/modules.go
@@ -1,60 +1,61 @@
 package modules
 
 import (
-    "git.lukas.moe/sn0w/Karen/modules/plugins"
-    "git.lukas.moe/sn0w/Karen/modules/triggers"
+	"git.lukas.moe/sn0w/Karen/modules/plugins"
+	"git.lukas.moe/sn0w/Karen/modules/triggers"
 )
 
 var (
-    pluginCache  map[string]*Plugin
-    triggerCache map[string]*TriggerPlugin
+	pluginCache  map[string]*Plugin
+	triggerCache map[string]*TriggerPlugin
 
-    PluginList = []Plugin{
-        &plugins.About{},
-        &plugins.Avatar{},
-        &plugins.Calc{},
-        &plugins.Changelog{},
-        &plugins.Choice{},
-        &plugins.FlipCoin{},
-        &plugins.Giphy{},
-        &plugins.Google{},
-        &plugins.Leet{},
-        &plugins.ListenDotMoe{},
-        &plugins.Minecraft{},
-        &plugins.Music{},
-        &plugins.Osu{},
-        &plugins.Ping{},
-        &plugins.RandomCat{},
-        &plugins.Ratelimit{},
-        &plugins.Reminders{},
-        &plugins.Roll{},
-        &plugins.RPS{},
-        &plugins.Stats{},
-        &plugins.Stone{},
-        &plugins.Support{},
-        &plugins.Announcement{},
-        //&plugins.Translator{},
-        &plugins.Uptime{},
-        &plugins.UrbanDict{},
-        &plugins.Weather{},
-        &plugins.XKCD{},
-    }
+	PluginList = []Plugin{
+		&plugins.About{},
+		&plugins.Avatar{},
+		&plugins.Calc{},
+		&plugins.Changelog{},
+		&plugins.Choice{},
+		&plugins.FlipCoin{},
+		&plugins.Giphy{},
+		&plugins.Google{},
+		&plugins.Headpat{},
+		&plugins.Leet{},
+		&plugins.ListenDotMoe{},
+		&plugins.Minecraft{},
+		&plugins.Music{},
+		&plugins.Osu{},
+		&plugins.Ping{},
+		&plugins.RandomCat{},
+		&plugins.Ratelimit{},
+		&plugins.Reminders{},
+		&plugins.Roll{},
+		&plugins.RPS{},
+		&plugins.Stats{},
+		&plugins.Stone{},
+		&plugins.Support{},
+		&plugins.Announcement{},
+		//&plugins.Translator{},
+		&plugins.Uptime{},
+		&plugins.UrbanDict{},
+		&plugins.Weather{},
+		&plugins.XKCD{},
+	}
 
-    TriggerPluginList = []TriggerPlugin{
-        &triggers.CSS{},
-        &triggers.Donate{},
-        &triggers.Git{},
-        &triggers.EightBall{},
-        &triggers.Hi{},
-        &triggers.HypeTrain{},
-        &triggers.Invite{},
-        &triggers.IPTables{},
-        &triggers.Lenny{},
-        &triggers.Nep{},
-        &triggers.Kawaii{},
-        &triggers.ReZero{},
-        &triggers.Shrug{},
-        &triggers.TableFlip{},
-        &triggers.Triggered{},
-    }
+	TriggerPluginList = []TriggerPlugin{
+		&triggers.CSS{},
+		&triggers.Donate{},
+		&triggers.Git{},
+		&triggers.EightBall{},
+		&triggers.Hi{},
+		&triggers.HypeTrain{},
+		&triggers.Invite{},
+		&triggers.IPTables{},
+		&triggers.Lenny{},
+		&triggers.Nep{},
+		&triggers.Kawaii{},
+		&triggers.ReZero{},
+		&triggers.Shrug{},
+		&triggers.TableFlip{},
+		&triggers.Triggered{},
+	}
 )
