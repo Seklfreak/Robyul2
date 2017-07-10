acb8378a0673f533f2f9cc37160ed6954d2afa2e
Sebastian Winkler <sekl@slmn.de>
2017-07-10 17:31:52 +0200

    [troublemaker] adds [p]troublemaker list command

--- modules/plugins/troublemaker.go
+++ modules/plugins/troublemaker.go
@@ -79,6 +79,57 @@ func (t *Troublemaker) Action(command string, content string, msg *discordgo.Mes
                 return
             })
             break
+        case "list":
+            helpers.RequireMod(msg, func() {
+                session.ChannelTyping(msg.ChannelID)
+
+                if len(args) < 2 {
+                    session.ChannelMessageSend(msg.ChannelID, helpers.GetText("bot.arguments.invalid"))
+                    return
+                }
+
+                targetUser, err := helpers.GetUserFromMention(args[1])
+                if err != nil || targetUser.ID == "" {
+                    session.ChannelMessageSend(msg.ChannelID, helpers.GetText("bot.arguments.invalid"))
+                    return
+                }
+
+                troublemakerReports := t.getTroublemakerReports(targetUser)
+
+                if len(troublemakerReports) <= 0 {
+                    _, err := session.ChannelMessageSend(msg.ChannelID, helpers.GetTextF("plugins.troublemaker.list-no-reports", targetUser.Username))
+                    helpers.Relax(err)
+                    return
+                } else {
+                    troublemakerText := fmt.Sprintf("I found %d report(s) for `%s#%s` `#%s` <@%s>:\n",
+                        len(troublemakerReports), targetUser.Username, targetUser.Discriminator, targetUser.ID, targetUser.ID,
+                    )
+
+                    for _, troublemakerReport := range troublemakerReports {
+                        reportedByUser, err := session.User(troublemakerReport.ReportedByUserID)
+                        if err != nil {
+                            reportedByUser = new(discordgo.User)
+                            reportedByUser.ID = troublemakerReport.ReportedByUserID
+                            reportedByUser.Username = "N/A"
+                        }
+                        reportedByGuild, err := session.Guild(troublemakerReport.ReportedByGuildID)
+                        if err != nil {
+                            reportedByGuild = new(discordgo.Guild)
+                            reportedByGuild.ID = troublemakerReport.ReportedByGuildID
+                            reportedByGuild.Name = "N/A"
+                        }
+                        troublemakerText += fmt.Sprintf("At: `%s`, Reason: `%s`, Reported By: `%s` (`#%s`) On: `%s` (`#%s`)\n",
+                            troublemakerReport.CreatedAt.Format(time.ANSIC), troublemakerReport.Reason,
+                            reportedByUser.Username, reportedByUser.ID, reportedByGuild.Name, reportedByGuild.ID,
+                        )
+                    }
+
+                    for _, page := range helpers.Pagify(troublemakerText, "\n") {
+                        _, err := session.ChannelMessageSend(msg.ChannelID, page)
+                        helpers.Relax(err)
+                    }
+                }
+            })
         default:
             helpers.RequireMod(msg, func() {
                 session.ChannelTyping(msg.ChannelID)
@@ -198,6 +249,18 @@ func (t *Troublemaker) Action(command string, content string, msg *discordgo.Mes
             break
         }
     }
+}
+
+func (t *Troublemaker) getTroublemakerReports(user *discordgo.User) []DB_Troublemaker_Entry {
+    var entryBucket []DB_Troublemaker_Entry
+    listCursor, err := rethink.Table("troublemakerlog").Filter(
+        rethink.Row.Field("userid").Eq(user.ID),
+    ).Run(helpers.GetDB())
+    helpers.Relax(err)
+    defer listCursor.Close()
+    listCursor.All(&entryBucket)
+
+    return entryBucket
 }
 
 func (t *Troublemaker) getEntryByOrCreateEmpty(key string, id string) DB_Troublemaker_Entry {
