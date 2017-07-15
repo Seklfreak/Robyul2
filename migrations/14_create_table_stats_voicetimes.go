112666937e4f03c73c9611533db30d7857d811f3
Sebastian Winkler <sekl@slmn.de>
2017-07-16 00:07:02 +0200

    [stats] adds voicestats db indexes

--- migrations/14_create_table_stats_voicetimes.go
+++ migrations/14_create_table_stats_voicetimes.go
@@ -1,5 +1,13 @@
 package migrations
 
+import (
+    rethink "github.com/gorethink/gorethink"
+    "github.com/Seklfreak/Robyul2/helpers"
+)
+
 func m14_create_table_stats_voicetimes() {
     CreateTableIfNotExists("stats_voicetimes")
+
+    rethink.Table("stats_voicetimes").IndexCreate("guildid").Run(helpers.GetDB())
+    rethink.Table("stats_voicetimes").IndexCreate("userid").Run(helpers.GetDB())
 }
