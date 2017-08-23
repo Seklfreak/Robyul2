e49c7231c85792571fa31282d1ccf30f09338524
chanyoung <park910113@gmail.com>
2017-08-23 15:54:12 +0900

    [youtube] enable keyword, channel search

--- modules/plugins/youtube.go
+++ modules/plugins/youtube.go
@@ -1,14 +1,14 @@
 package plugins
 
 import (
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
 
+	"fmt"
+
 	"github.com/Seklfreak/Robyul2/helpers"
 	"github.com/bwmarrin/discordgo"
-	"github.com/dustin/go-humanize"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	"golang.org/x/oauth2/jwt"
@@ -52,77 +52,66 @@ func (yt *YouTube) Init(session *discordgo.Session) {
 func (yt *YouTube) Action(command string, content string, msg *discordgo.Message, session *discordgo.Session) {
 	defer helpers.Recover()
 
-	var err error
-	var reply *discordgo.MessageSend
-
 	session.ChannelTyping(msg.ChannelID)
 
 	args := strings.Fields(content)
 	if len(args) < 1 {
-		_, err = session.ChannelMessageSend(msg.ChannelID, helpers.GetText("bot.arguments.invalid"))
+		_, err := session.ChannelMessageSend(msg.ChannelID, helpers.GetText("bot.arguments.invalid"))
 		helpers.Relax(err)
 		return
 	}
 
-	// switch to subcommand.
+	var result *discordgo.MessageSend
 	switch args[0] {
 	default:
 		// _youtube {args[0]: videoID}
-		reply, err = yt.getVideo(args[0:])
+		result = yt.search(args[0:])
 	}
 
-	// sends error messages to client.
-	if err != nil {
-		_, err = session.ChannelMessageSend(msg.ChannelID, helpers.GetText(err.Error()))
-		helpers.Relax(err)
-		return
-	}
-
-	// sends result to client.
-	_, err = session.ChannelMessageSendComplex(msg.ChannelID, reply)
+	_, err := session.ChannelMessageSendComplex(msg.ChannelID, result)
 	helpers.Relax(err)
 }
 
-func (yt *YouTube) getVideo(args []string) (*discordgo.MessageSend, error) {
+func (yt *YouTube) search(args []string) *discordgo.MessageSend {
 	if yt.service == nil {
-		return nil, fmt.Errorf("plugins.youtube.service-not-available")
+		return &discordgo.MessageSend{Content: "plugins.youtube.service-not-available"}
 	}
 
 	// _youtube {args[0]: videoID}
 	if len(args) < 1 {
-		return nil, fmt.Errorf("bot.arguments.invalid")
+		return &discordgo.MessageSend{Content: "bot.argument.invalid"}
 	}
-	videoID := args[0]
+	query := strings.Join(args, " ")
 
-	response, err := yt.service.Videos.List("id, snippet, statistics").Id(videoID).MaxResults(1).Do()
+	call := yt.service.Search.List("id,snippet").
+		Q(query).
+		Type("channel,video").
+		SafeSearch("strict").
+		MaxResults(1)
+
+	response, err := call.Do()
 	if err != nil {
-		return nil, err
+		return &discordgo.MessageSend{Content: err.Error()}
 	}
+
 	if len(response.Items) <= 0 {
-		return nil, fmt.Errorf("plugins.youtube.video-not-found")
+		return &discordgo.MessageSend{Content: "plugins.youtube.video-not-found"}
+	}
+	item := response.Items[0]
+
+	var id, url string
+	switch item.Id.Kind {
+	case "youtube#video":
+		id = item.Id.VideoId
+		url = fmt.Sprintf(YouTubeVideoBaseUrl, id)
+	case "youtube#channel":
+		id = item.Id.ChannelId
+		url = fmt.Sprintf(YouTubeChannelBaseUrl, id)
+	default:
+		return &discordgo.MessageSend{Content: "unknown item kind: " + item.Kind}
 	}
 
-	video := response.Items[0]
-
-	videoEmbed := &discordgo.MessageEmbed{
-		Footer: &discordgo.MessageEmbedFooter{Text: "YouTube"},
-		Author: &discordgo.MessageEmbedAuthor{
-			Name: video.Snippet.ChannelTitle,
-			URL:  fmt.Sprintf(YouTubeChannelBaseUrl, video.Snippet.ChannelId)},
-		Title: video.Snippet.Title,
-		URL:   fmt.Sprintf(YouTubeVideoBaseUrl, video.Id),
-		Image: &discordgo.MessageEmbedImage{URL: video.Snippet.Thumbnails.High.Url},
-		Fields: []*discordgo.MessageEmbedField{
-			{Name: "Views", Value: humanize.Comma(int64(video.Statistics.ViewCount)), Inline: true},
-			{Name: "Likes", Value: humanize.Comma(int64(video.Statistics.LikeCount)), Inline: true},
-		},
-		Color: helpers.GetDiscordColorFromHex(YouTubeColor),
-	}
-
-	return &discordgo.MessageSend{
-		Content: fmt.Sprintf("<%s>", fmt.Sprintf(YouTubeVideoBaseUrl, video.Id)),
-		Embed:   videoEmbed,
-	}, nil
+	return &discordgo.MessageSend{Content: url}
 }
 
 func (yt *YouTube) createConfig() error {
