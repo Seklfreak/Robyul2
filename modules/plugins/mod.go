11061b3c9a31c8585c62a1f0b71fb67b28f22c76
Sebastian Winkler <sebastian.winkler@slmn.de>
2017-10-02 17:04:37 +0200

    [mod] displays time when user get unmuted at timed mutes

--- modules/plugins/mod.go
+++ modules/plugins/mod.go
@@ -486,16 +486,19 @@ func (m *Mod) Action(command string, content string, msg *discordgo.Message, ses
 					helpers.Relax(err)
 				}
 
+				successText := helpers.GetTextF("plugins.mod.user-muted-success", targetUser.Username, targetUser.ID)
+
 				if time.Now().Before(timeToUnmuteAt) {
 					signature := helpers.UnmuteUserSignature(channel.GuildID, targetUser.ID)
 					signature.ETA = &timeToUnmuteAt
 
 					_, err = cache.GetMachineryServer().SendTask(signature)
 					helpers.Relax(err)
+
+					successText = helpers.GetTextF("plugins.mod.user-muted-success-timed", targetUser.Username, targetUser.ID, timeToUnmuteAt.Format(time.ANSIC)+" UTC")
 				}
 
-				// TODO: update text
-				_, err = session.ChannelMessageSend(msg.ChannelID, helpers.GetTextF("plugins.mod.user-muted-success", targetUser.Username, targetUser.ID))
+				_, err = session.ChannelMessageSend(msg.ChannelID, successText)
 				helpers.Relax(err)
 				return
 			} else {
