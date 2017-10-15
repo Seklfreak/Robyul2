5378d0a025fb79636566e41bc7689a1ae98af1f8
chanyoung <park910113@gmail.com>
2017-10-15 15:46:32 +0900

    [youtube] fixes name of test

--- modules/plugins/youtube/common_test.go
+++ modules/plugins/youtube/common_test.go
@@ -4,15 +4,13 @@ import "testing"
 import "github.com/bwmarrin/discordgo"
 
 func TestVerifyEmbedFields(t *testing.T) {
-	testYoutube := YouTube{}
-
 	fields := []*discordgo.MessageEmbedField{
 		{Name: "", Value: "value"},
 		{Name: "name", Value: ""},
 		{Name: "name", Value: "0"},
 	}
 
-	fields = testYoutube.verifyEmbedFields(fields)
+	fields = verifyEmbedFields(fields)
 	if len(fields) != 0 {
 		t.Fatalf("youtube.verifyEmbedFields() failed to trim invalid field")
 	}
