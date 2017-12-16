79ceba8b8739205c8d34af25ae61ab48c764c413
Sebastian Winkler <sebastian.winkler@slmn.de>
2017-12-16 12:57:57 +0100

    [ping] adds more information

--- modules/plugins/ping.go
+++ modules/plugins/ping.go
@@ -6,8 +6,12 @@ import (
 
 	"strings"
 
+	"context"
+
+	"github.com/Seklfreak/Robyul2/cache"
 	"github.com/Seklfreak/Robyul2/helpers"
 	"github.com/bwmarrin/discordgo"
+	"github.com/gorethink/gorethink"
 )
 
 type Ping struct{}
@@ -50,13 +54,30 @@ func (p *Ping) OnMessage(session *discordgo.Session, message *discordgo.MessageC
 
 	gatewayTaken := time.Duration(time.Now().UnixNano() - parsedUnixNano)
 
-	text := strings.Replace(message.Content, " ~ "+textUnixNano, "", 1) + "\nGateway Time: " + gatewayTaken.String()
+	text := strings.Replace(message.Content, " ~ "+textUnixNano, "", 1) + "\nGateway Latency (receive message): " + gatewayTaken.String()
 
 	started := time.Now()
 	helpers.EditMessage(message.ChannelID, message.ID, text)
 	apiTaken := time.Since(started)
 
-	text = text + "\nHTTP API Time (edit message): " + apiTaken.String()
-	helpers.EditMessage(message.ChannelID, message.ID, text)
+	text = text + "\nHTTP API Latency (edit message): " + apiTaken.String()
 
+	started = time.Now()
+	gorethink.Expr(1).Run(helpers.GetDB())
+	rethinkTaken := time.Since(started)
+	text = text + "\nRethinkDB Latency: " + rethinkTaken.String()
+
+	started = time.Now()
+	cache.GetRedisClient().Ping()
+	redisTaken := time.Since(started)
+	text = text + "\nRedis Latency: " + redisTaken.String()
+
+	if helpers.GetConfig().Path("elasticsearch.url").Data().(string) != "" {
+		started = time.Now()
+		cache.GetElastic().Ping(helpers.GetConfig().Path("elasticsearch.url").Data().(string)).Do(context.Background())
+		elasticTaken := time.Since(started)
+		text = text + "\nElasticSearch Latency: " + elasticTaken.String()
+	}
+
+	helpers.EditMessage(message.ChannelID, message.ID, text)
 }
