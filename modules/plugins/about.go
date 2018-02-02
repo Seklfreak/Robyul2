e8f0c2504e647ee22102017520c24cbd7f8e4293
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-02-02 20:25:35 +0100

    [about] improves about

--- modules/plugins/about.go
+++ modules/plugins/about.go
@@ -1,6 +1,10 @@
 package plugins
 
 import (
+	"strings"
+
+	"fmt"
+
 	"github.com/Seklfreak/Robyul2/helpers"
 	"github.com/bwmarrin/discordgo"
 )
@@ -19,7 +23,58 @@ func (a *About) Init(session *discordgo.Session) {
 }
 
 func (a *About) Action(command string, content string, msg *discordgo.Message, session *discordgo.Session) {
-	m := "**Hey! I'm Robyul.**\nI'm built using Go, open-source and a fork of Shiro, formerly called Karen, which you can find here: <https://github.com/SubliminalHQ/shiro>.\nYou can find out more about me here: <https://robyul.chat/>.\nSuggestions and discussions are always welcome on the Discord for me: <https://discord.gg/s5qZvUV>."
+	channel, err := helpers.GetChannel(msg.ChannelID)
+	helpers.Relax(err)
 
-	helpers.SendMessage(msg.ChannelID, m)
+	embed := &discordgo.MessageEmbed{
+		URL:         "https://robyul.chat/",
+		Title:       "Hello, I'm Robyul!",
+		Description: "I'm built using Go, open-source and a fork of [Shiro](https://github.com/SubliminalHQ/shiro), formerly called Karen.",
+		Color:       0,
+		Author: &discordgo.MessageEmbedAuthor{
+			URL:     "https://robyul.chat/",
+			Name:    "Robyul - The KPop Discord Bot",
+			IconURL: session.State.User.AvatarURL("64"),
+		},
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name:   ":earth_asia: Website",
+				Value:  "https://robyul.chat/",
+				Inline: false,
+			},
+			{
+				Name:   ":tools: Source",
+				Value:  "https://github.com/Seklfreak/Robyul2",
+				Inline: false,
+			},
+			{
+				Name:   ":speech_left: Support Discord",
+				Value:  "https://discord.gg/s5qZvUV",
+				Inline: false,
+			},
+			{
+				Name:   ":railway_track: Trello",
+				Value:  "https://trello.robyul.chat/",
+				Inline: false,
+			},
+			{
+				Name:   ":vertical_traffic_light: Status",
+				Value:  "https://status.robyul.chat/",
+				Inline: false,
+			},
+			{
+				Name:   ":cop: Team",
+				Value:  strings.Replace(helpers.GetStaffUsernamesText(), "or", "and", -1),
+				Inline: false,
+			},
+			{
+				Name: ":bulb: Tip",
+				Value: fmt.Sprintf("Make suggestions for Robyul: `%ssuggest <your suggestion>`!",
+					helpers.GetPrefixForServer(channel.GuildID)),
+				Inline: false,
+			},
+		},
+	}
+
+	helpers.SendEmbed(msg.ChannelID, embed)
 }
