45ce1516b0ddcb97e643f0f03f8d2e979d0bb55c
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-03-13 17:52:36 +0100

    🕰 [levels] increases timeout time

--- helpers/screenshot.go
+++ helpers/screenshot.go
@@ -1,7 +1,11 @@
 package helpers
 
 import (
+	"bytes"
 	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
 var (
@@ -22,12 +26,24 @@ func TakeHTMLScreenshot(html string, width, height int) (data []byte, err error)
 		return nil, err
 	}
 
-	data, err = NetPostUAWithError(sushiiImageServerBase+"/html", string(marshalledRequest), DEFAULT_UA)
+	client := &http.Client{
+		Timeout: time.Duration(30 * time.Second),
+	}
+
+	request, err := http.NewRequest("POST", sushiiImageServerBase+"/html", bytes.NewBuffer(marshalledRequest))
 	if err != nil {
 		return nil, err
 	}
 
-	return data, nil
+	request.Header.Set("User-Agent", DEFAULT_UA)
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadAll(response.Body)
 }
 
 type SushiRequest struct {
