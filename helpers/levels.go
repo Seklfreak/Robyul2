ba7eba25dd3f222ca54bb7f4539c9cb4f80f5cfb
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-04-30 12:20:48 +0200

    [reminders] displays reminder time on creation ⏰

--- helpers/levels.go
+++ helpers/levels.go
@@ -1,5 +1,11 @@
 package helpers
 
+import (
+	"github.com/Seklfreak/Robyul2/models"
+	"github.com/globalsign/mgo/bson"
+	"gitlab.com/project-d-collab/dhelpers/mdb"
+)
+
 func GetMaxBadgesForGuild(guildID string) (maxBadges int) {
 	maxBadges = GuildSettingsGetCached(guildID).LevelsMaxBadges
 	if maxBadges == 0 {
@@ -9,4 +15,20 @@ func GetMaxBadgesForGuild(guildID string) (maxBadges int) {
 		maxBadges = 0
 	}
 	return maxBadges
+}
+
+func GetUserUserdata(userID string) (userdata models.ProfileUserdataEntry, err error) {
+	err = MdbOne(
+		MdbCollection(models.ProfileUserdataTable).Find(bson.M{"userid": userID}),
+		&userdata,
+	)
+
+	if mdb.ErrNotFound(err) {
+		userdata.UserID = userID
+		newid, err := MDbInsert(models.ProfileUserdataTable, userdata)
+		userdata.ID = newid
+		return userdata, err
+	}
+
+	return userdata, err
 }
