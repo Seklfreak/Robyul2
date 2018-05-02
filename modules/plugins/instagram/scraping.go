cf5808b8a884a98ff16b800dc68138eb7ff20c5e
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-05-02 02:12:54 +0200

    [instagram] fixes scraping 👀
    fixes ROBYUL-DISCORD-KB

--- modules/plugins/instagram/scraping.go
+++ modules/plugins/instagram/scraping.go
@@ -26,12 +26,11 @@ func (m *Handler) getInformationAndPosts(username string, proxy http.Transport) 
 	err = json.Unmarshal([]byte(sharedDataText), &graphQlFeedResult)
 	helpers.Relax(err)
 
-	if graphQlFeedResult.EntryData.ProfilePage == nil || len(graphQlFeedResult.EntryData.ProfilePage) < 0 ||
-		graphQlFeedResult.EntryData.ProfilePage[0].Graphql.User.ID == "" {
+	if graphQlFeedResult.EntryData.ProfilePage.Graphql.User.ID == "" {
 		return information, posts, errors.New("failed to find user information")
 	}
 
-	feed := graphQlFeedResult.EntryData.ProfilePage[0].Graphql
+	feed := graphQlFeedResult.EntryData.ProfilePage.Graphql
 
 	receivedPosts := feed.User.EdgeOwnerToTimelineMedia.Edges
 
@@ -84,41 +83,41 @@ func (m *Handler) getPostInformation(shortCode string, proxy http.Transport) (in
 		return
 	}
 
-	for _, postData := range sharedData.EntryData.PostPage {
-		if postData.Graphql.ShortcodeMedia.Shortcode == shortCode {
-			information = InstagramPostInformation{
-				ID:        postData.Graphql.ShortcodeMedia.ID + "_" + postData.Graphql.ShortcodeMedia.Owner.ID,
-				Shortcode: postData.Graphql.ShortcodeMedia.Shortcode,
-				Author: InstagramAuthorInformations{
-					ID:            postData.Graphql.ShortcodeMedia.Owner.ID,
-					ProfilePicUrl: postData.Graphql.ShortcodeMedia.Owner.ProfilePicURL,
-					Username:      postData.Graphql.ShortcodeMedia.Owner.Username,
-					FullName:      postData.Graphql.ShortcodeMedia.Owner.FullName,
-					IsPrivate:     postData.Graphql.ShortcodeMedia.Owner.IsPrivate,
-					IsVerified:    postData.Graphql.ShortcodeMedia.Owner.IsVerified,
-				},
-				MediaUrls: []string{m.getBestDisplayResource(postData.Graphql.ShortcodeMedia.DisplayResources)},
-				Caption:   "",
-				TakentAt:  time.Unix(int64(postData.Graphql.ShortcodeMedia.TakenAtTimestamp), 0),
-				IsVideo:   postData.Graphql.ShortcodeMedia.IsVideo,
-			}
-			if postData.Graphql.ShortcodeMedia.VideoURL != "" {
-				information.MediaUrls = []string{postData.Graphql.ShortcodeMedia.VideoURL}
-			}
-			if postData.Graphql.ShortcodeMedia.EdgeMediaToCaption.Edges != nil &&
-				len(postData.Graphql.ShortcodeMedia.EdgeMediaToCaption.Edges) > 0 {
-				information.Caption = postData.Graphql.ShortcodeMedia.EdgeMediaToCaption.Edges[0].Node.Text
-			}
-			if postData.Graphql.ShortcodeMedia.EdgeSidecarToChildren.Edges != nil &&
-				len(postData.Graphql.ShortcodeMedia.EdgeSidecarToChildren.Edges) > 0 {
-				information.MediaUrls = make([]string, 0)
-				for _, sidecar := range postData.Graphql.ShortcodeMedia.EdgeSidecarToChildren.Edges {
-					sidecarUrl := m.getBestDisplayResource(sidecar.Node.DisplayResources)
-					if sidecar.Node.VideoURL != "" {
-						sidecarUrl = sidecar.Node.VideoURL
-					}
-					information.MediaUrls = append(information.MediaUrls, sidecarUrl)
+	postData := sharedData.EntryData.PostPage
+
+	if postData.Graphql.ShortcodeMedia.Shortcode == shortCode {
+		information = InstagramPostInformation{
+			ID:        postData.Graphql.ShortcodeMedia.ID + "_" + postData.Graphql.ShortcodeMedia.Owner.ID,
+			Shortcode: postData.Graphql.ShortcodeMedia.Shortcode,
+			Author: InstagramAuthorInformations{
+				ID:            postData.Graphql.ShortcodeMedia.Owner.ID,
+				ProfilePicUrl: postData.Graphql.ShortcodeMedia.Owner.ProfilePicURL,
+				Username:      postData.Graphql.ShortcodeMedia.Owner.Username,
+				FullName:      postData.Graphql.ShortcodeMedia.Owner.FullName,
+				IsPrivate:     postData.Graphql.ShortcodeMedia.Owner.IsPrivate,
+				IsVerified:    postData.Graphql.ShortcodeMedia.Owner.IsVerified,
+			},
+			MediaUrls: []string{m.getBestDisplayResource(postData.Graphql.ShortcodeMedia.DisplayResources)},
+			Caption:   "",
+			TakentAt:  time.Unix(int64(postData.Graphql.ShortcodeMedia.TakenAtTimestamp), 0),
+			IsVideo:   postData.Graphql.ShortcodeMedia.IsVideo,
+		}
+		if postData.Graphql.ShortcodeMedia.VideoURL != "" {
+			information.MediaUrls = []string{postData.Graphql.ShortcodeMedia.VideoURL}
+		}
+		if postData.Graphql.ShortcodeMedia.EdgeMediaToCaption.Edges != nil &&
+			len(postData.Graphql.ShortcodeMedia.EdgeMediaToCaption.Edges) > 0 {
+			information.Caption = postData.Graphql.ShortcodeMedia.EdgeMediaToCaption.Edges[0].Node.Text
+		}
+		if postData.Graphql.ShortcodeMedia.EdgeSidecarToChildren.Edges != nil &&
+			len(postData.Graphql.ShortcodeMedia.EdgeSidecarToChildren.Edges) > 0 {
+			information.MediaUrls = make([]string, 0)
+			for _, sidecar := range postData.Graphql.ShortcodeMedia.EdgeSidecarToChildren.Edges {
+				sidecarUrl := m.getBestDisplayResource(sidecar.Node.DisplayResources)
+				if sidecar.Node.VideoURL != "" {
+					sidecarUrl = sidecar.Node.VideoURL
 				}
+				information.MediaUrls = append(information.MediaUrls, sidecarUrl)
 			}
 		}
 	}
