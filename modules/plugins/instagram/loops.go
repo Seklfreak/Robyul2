e80ef8de34013a8f030ffc129e49a65dd9a4d1f9
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-05-10 14:54:25 +0200

    [instagram] switch proxy on 503 error 💣

--- modules/plugins/instagram/loops.go
+++ modules/plugins/instagram/loops.go
@@ -347,7 +347,8 @@ func (m *Handler) retryOnError(err error) (retry bool) {
 			strings.Contains(err.Error(), "expected status 200; got 429") ||
 			strings.Contains(err.Error(), "Please wait a few minutes before you try again.") ||
 			strings.Contains(err.Error(), "expected status 200; got 500") ||
-			strings.Contains(err.Error(), "expected status 200; got 502") {
+			strings.Contains(err.Error(), "expected status 200; got 502") ||
+			strings.Contains(err.Error(), "expected status 200; got 503") {
 			return true
 		}
 	}
