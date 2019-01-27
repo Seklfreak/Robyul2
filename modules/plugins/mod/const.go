33d85e4327f2da7170184d69dd1f141a76e198cb
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-01-27 18:56:14 +0100

    [mod] toggles user ban inspect default value 🔨

--- modules/plugins/mod/const.go
+++ modules/plugins/mod/const.go
@@ -2,5 +2,5 @@ package mod
 
 const (
 	featureFlagInspectUserGotBanned         = "module-mod-feature-inspect-user-got-banned"
-	featureFlagInspectUserGotBannedFallback = true
+	featureFlagInspectUserGotBannedFallback = false
 )
